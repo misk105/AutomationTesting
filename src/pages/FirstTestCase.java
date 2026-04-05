f60f556cf118fab5b9cb6d665f021df46638482e
misk <misk.haneef@gmail.com>
2026-04-05 11:30:25 +0200

    fourth commit

--- src/pages/FirstTestCase.java
+++ src/pages/FirstTestCase.java
@@ -3,28 +3,40 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FirstTestCase {
 
     private WebDriver driver;
 
-    @BeforeMethod
+    @BeforeTest
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", ConfigReader.getChromePath());
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    @AfterMethod
+    @AfterTest
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
     }
 
+    @Test
+    public void testHomePageLoaded() {
+    	String url      = ConfigReader.getAppUrl();
+        HomePage homePage = new HomePage(driver);
+        
+        // Step 1: Navigate to the application
+        driver.get(url);
+
+        // Step 2: Verify homepage elements are displayed
+        homePage.verifyHomePageLoaded();
+    }
+    
     @Test
     public void testLogin() {
 
@@ -35,12 +47,6 @@ public class FirstTestCase {
         HomePage    homePage    = new HomePage(driver);
         LoginPage   loginPage   = new LoginPage(driver);
         AccountPage accountPage = new AccountPage(driver);
-
-        // Step 1: Navigate to the application
-        driver.get(url);
-
-        // Step 2: Verify homepage elements are displayed
-        homePage.verifyHomePageLoaded();
         
         // Step 3: Open the sign-in flow
         homePage.goToLoginPage();
